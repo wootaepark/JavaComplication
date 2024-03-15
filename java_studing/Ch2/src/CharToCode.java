5de49bd4e8c60f957e8af9f2e0797dab79865f6f
wootaepark <pakdal99@gmail.com>
2024-03-15 22:46:46 +0900

    java_studing_1

---
+++ java_studing/Ch2/src/CharToCode.java
@@ -0,0 +1,16 @@
+
+public class CharToCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'A';
+		int code = (int)ch;
+		
+		System.out.printf("%c = %d(%#X)%n", ch, code, code);
+		
+		char hch = '가';
+		System.out.printf("%c=%d(%#X)%n", hch,(int)hch, (int)hch);
+
+	}
+
+}
---
+++ java_studing/Ch2/src/SpecailCharEx.java
@@ -0,0 +1,14 @@
+
+public class SpecailCharEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('\''); // '를 출력하기
+		System.out.println("abc\t123\b456");
+		System.out.println('\n');
+		System.out.println("\"Hello\"");
+		System.out.println("c:\\");
+
+	}
+
+}
