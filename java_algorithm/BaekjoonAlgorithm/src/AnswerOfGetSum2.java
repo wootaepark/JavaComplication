f8e7f01def3530c1c6fd9a12aba5c3f9e1b6bad0
wootaepark <pakdal99@gmail.com>
2024-03-25 15:49:20 +0900

    BaekJoon11660-commit

---
+++ java_algorithm/BaekjoonAlgorithm/src/AnswerOfGetSum2.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+public class AnswerOfGetSum2 {
+	// 백준 11660 문제의 dp 알고리즘 정답
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int size = Integer.parseInt(st.nextToken());
+		int count = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[size+1][size+1];
+		int[][] sum = new int[size+1][size+1];
+		
+		
+		for(int i = 1;i<size+1;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1;j<size+1;j++) {
+				
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				
+			}
+		}
+	
+		
+		for(int i=1;i<size+1;i++) {
+			for(int j=1;j<size+1;j++) {
+				sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + arr[i][j];
+			
+			}
+		}
+		
+		int[] result = new int[count];
+		
+		for(int t=0;t<count;t++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			
+			result[t] = sum[c][d] - sum[a-1][d] - sum[c][b-1] + sum[a-1][b-1];
+			
+			
+		}
+		
+		for(int i=0;i<result.length;i++)
+			System.out.println(result[i]);
+		
+		
+	
+		
+		br.close();
+		bw.flush();
+		bw.close();
+		
+
+	}
+
+}
---
+++ java_algorithm/BaekjoonAlgorithm/src/GetSum2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class GetSum2 {
+	// 백준 11660번, 시간 초과 코드
+	// 일반적인 for문 이용 (시간복잡도 이용)
+	// dp 로 풀어보기
+	
+	static int getSum(int arr[][],int sRow, int sCol, int eRow, int eCol) {
+		
+		int result = 0;
+		for(int i=sRow;i<=eRow;i++) {
+			for(int j=sCol;j<=eCol;j++) {
+			result+=arr[i][j];
+			}
+		}
+		
+		return result;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		// 입력
+		int size = scanner.nextInt();
+		int getCount = scanner.nextInt();
+		
+		int[][] data = new int[size][size];
+		
+		for(int i=0;i<data.length;i++) {
+			for(int j=0;j<data[0].length;j++) {
+				data[i][j] = scanner.nextInt();
+			}
+		}
+		
+		// 테이블 배열 데이터 입력 끝
+		
+		int sR, sC, eR, eC;
+		
+		int result[] = new int[getCount];
+		
+		for(int k =0;k<getCount;k++) {
+			sR = scanner.nextInt()-1;
+			sC = scanner.nextInt()-1;
+			eR = scanner.nextInt()-1;
+			eC = scanner.nextInt()-1;
+			result[k] = getSum(data,sR,sC,eR,eC);
+		}
+		
+		for(int t=0;t<result.length;t++) {
+			System.out.println(result[t]);
+		}
+		
+		
+		
+		
+		
+		
+		scanner.close();
+	}
+
+}
