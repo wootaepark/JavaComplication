6d2eba31406696183aeb199214e6218fbda7830b
wootaepark <pakdal99@gmail.com>
2024-05-22 20:07:16 +0900

    01Tile_01-commit

---
+++ java_algorithm/BaekjoonAlgorithm/src/_01Tile.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+// 백준 1094 문제 : 01타일
+public class _01Tile {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+		br.close();
+
+	}
+
+}
