bf08383a64189dfc250286a322c58b5dc6a30526
wootaepark <pakdal99@gmail.com>
2024-05-26 21:44:25 +0900

    09_assignment_01-commit

---
+++ java_lecture/09_assignment/src/PointManage.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class PointManage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		scanner.close();
+
+	}
+
+}
