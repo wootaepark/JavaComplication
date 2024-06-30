3a141d18dfef6e456c9cb99e56c6b86c19876a93
wootaepark <pakdal99@gmail.com>
2024-06-30 20:43:36 +0900

    BaekJoon_1904-commit

--- java_algorithm/BaekjoonAlgorithm/src/_01Tile.java
+++ java_algorithm/BaekjoonAlgorithm/src/_01Tile.java
@@ -1,18 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-// 백준 1094 문제 : 01타일 // 추후 풀이 예정
+// 백준 1904 문제 : 01타일 // 추후 풀이 예정
 public class _01Tile {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		
-		br.close();
+		long[] size = new long[N];
 
+		
+		size[0] = 1;
+		if(N >= 2) {
+			size[1] = 2;
+		}
+		
+		if(N>2) {
+			for(int i=2; i<N; i++) {
+
+				size[i] = (size[i-1] + size[i-2]) % 15746;
+			
+				// 해당 알고리즘 찾는것이 핵심이다.
+		}
+			
+		}
+		
+		System.out.println(size[N-1]); // 오버플로우 방지
+		
+		scanner.close();
 	}
 
 }
