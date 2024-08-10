41c60ea3d16157f9539fa1506abbea223599c2bb
wootaepark <pakdal99@gmail.com>
2024-08-10 17:26:20 +0900

    temp commit 2

--- java_algorithm/BaekjoonAlgorithm/src/BaekJoon_1940.java
+++ java_algorithm/BaekjoonAlgorithm/src/BaekJoon_1940.java
@@ -3,6 +3,11 @@ public class BaekJoon_1940 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+		scanner.close();
 
 	}
 
