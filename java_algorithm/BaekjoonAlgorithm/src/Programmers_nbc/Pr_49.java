b4651e2203e896f0cb9eb43a041bb3c8a87898d1
wootaepark <pakdal99@gmail.com>
2024-08-28 20:12:26 +0900

    nbc java codekata 49

---
+++ java_algorithm/BaekjoonAlgorithm/src/Programmers_nbc/Pr_49.java
@@ -0,0 +1,26 @@
+package Programmers_nbc;
+
+import java.util.*;
+
+public class Pr_49 {
+	static class Solution {
+		public static int[] solution(int[] numbers) {
+			Set<Integer> productSet = new HashSet<>();
+			for (int i = 0; i < numbers.length - 1; i++) {
+				for (int j = i + 1; j < numbers.length; j++) {
+					int product = numbers[i] + numbers[j];
+					productSet.add(product); 
+				}
+			}
+			int[] answer = productSet.stream().mapToInt(Integer::intValue).toArray();
+			Arrays.sort(answer); 
+
+			return answer;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = {2, 1, 3, 4, 1};
+		System.out.println(Arrays.toString(Solution.solution(numbers)));
+	}
+}
